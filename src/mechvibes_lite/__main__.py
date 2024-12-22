1fd0a87ddee3d570a2ac8d1d0f84f9888624041b
myxi <myxi@envs.net>
2024-12-22 22:39:28 +0530

    chore: reformat files to adhere to pep8

--- src/mechvibes_lite/__main__.py
+++ src/mechvibes_lite/__main__.py
@@ -1,7 +1,7 @@
-import asyncio
-import pathlib
 import argparse
+import asyncio
 import logging
+import pathlib
 import threading
 
 import pyglet.app
@@ -19,7 +19,9 @@ async def daemon(theme_path, wskey_host, wskey_port) -> None:
     theme = struct.Theme.from_config(theme_path / "config.json", theme_path)
     keyplayer = audio.KeyPlayer(theme)
 
-    async for websocket in connect(f"ws://{wskey_host}:{wskey_port}", ping_timeout=None):
+    async for websocket in connect(
+        f"ws://{wskey_host}:{wskey_port}", ping_timeout=None
+    ):
         try:
             async for message in websocket:
                 code = int(message)
@@ -37,7 +39,11 @@ def cmd_daemon(args) -> None:
         const.THEME_FOLDER_NAME = args.theme_folder_name
         const.THEME_PATH = const.THEME_DIR / const.THEME_FOLDER_NAME
     pyglet.options["headless"] = True
-    thread = threading.Thread(target=asyncio.run, args=[daemon(const.THEME_PATH, args.wskey_host, args.wskey_port)], daemon=True)
+    thread = threading.Thread(
+        target=asyncio.run,
+        args=[daemon(const.THEME_PATH, args.wskey_host, args.wskey_port)],
+        daemon=True,
+    )
     thread.start()
     pyglet.app.run()
 
@@ -53,15 +59,17 @@ def main() -> None:
     )
     subparsers = parser.add_subparsers()
 
-    subparsers.add_parser(
-        "wskey", parents=[wskey_parser], conflict_handler="resolve"
-    )
+    subparsers.add_parser("wskey", parents=[wskey_parser], conflict_handler="resolve")
 
     subparser_daemon = subparsers.add_parser("daemon")
     subparser_daemon.add_argument("--wskey-host", default=const.WSKEY_HOST)
     subparser_daemon.add_argument("--wskey-port", default=const.WSKEY_PORT)
-    subparser_daemon.add_argument("--theme-dir", default=const.THEME_DIR, type=pathlib.Path)
-    subparser_daemon.add_argument("--theme-folder-name", default=const.THEME_FOLDER_NAME, type=pathlib.Path)
+    subparser_daemon.add_argument(
+        "--theme-dir", default=const.THEME_DIR, type=pathlib.Path
+    )
+    subparser_daemon.add_argument(
+        "--theme-folder-name", default=const.THEME_FOLDER_NAME, type=pathlib.Path
+    )
     subparser_daemon.set_defaults(func=cmd_daemon)
 
     args = parser.parse_args()
