6ea8c57230494333383d2c966ee08ce73690242f
myxi <myxi@envs.net>
2025-04-01 15:28:25 +0530

    feat: move a few things that should be constants in the const.py file

--- src/mechvibes_lite/const.py
+++ src/mechvibes_lite/const.py
@@ -1,7 +1,12 @@
 import kisesi
 
+from mechvibes_lite import util
+
 log = kisesi.get_logger(__name__)
 
 APP_NAME = "mechvibes-lite"
 APP_DESCRIPTION = "Mechvibes Lite is an alternative to Mechvibes (it plays sounds when you press keys."
 APP_EPILOG = " * Homepage for Mechvibes Lite is hosted at https://github.com/eeriemyxi/mechvibes-lite "
+
+CONFIG_HOME = util.get_config_path(APP_NAME)
+CONFIG_PATH = CONFIG_HOME / "config.ini"
